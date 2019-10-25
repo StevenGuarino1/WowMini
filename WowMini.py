24edac820af2bbfd57f0078140e0f9cf8322c87d
StevenGuarino <guarinosteven50@gmail.com>
2019-10-25 15:23:18 -0400

    final updates

--- WowMini.py
+++ WowMini.py
@@ -1,8 +1,6 @@
 import pygame
+import time
 from pygame.locals import *
-import os
-import sys
-import math
 
 RED = [255, 0, 0]
 GREEN = [0, 255, 0]
@@ -10,25 +8,36 @@ BLUE = [0, 0, 255]
 BLACK = [0, 0, 0]
 WHITE = [255, 255, 255]
 
-sprite_w = 20
-sprite_h = 60
+sprite_width = 20
+sprite_height = 60
 
 y_boundary = 0
 
 ground_y = 293
 
-W, H = 800, 400
-win = pygame.display.set_mode((W, H))
-pygame.display.set_caption('Side Scroller')
+screen_width = 800
+screen_height = 400
+screen = pygame.display.set_mode((screen_width, screen_height))
 
-velocity = list([-9.5, -9, -8.5, -8, -7.5, -7, -6.5, -6, -5.5, -5, -4.5, -4, -2.5, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5])
+pygame.display.set_caption('Wow Mini')
+
+jump_speed = list([-9.5, -9, -8.5, -8, -7.5, -7, -6.5, -6, -5.5, -5,
+                   -4.5, -4, -2.5, -1.5, -1, -0.5, 0, 0.5, 1, 1.5,
+                   2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5])
+
 
 class Player(pygame.sprite.Sprite):
+    """
+    Creates a player instance
+
+
+    """
     def __init__(self, x, y):
         super().__init__()
 
-        self.image = pygame.Surface([sprite_w, sprite_h])
+        self.image = pygame.Surface([sprite_width, sprite_height])
         self.image.fill(RED)
+
         # Gets rect object for collision detection
         self.rect = self.image.get_rect()
 
@@ -39,32 +48,30 @@ class Player(pygame.sprite.Sprite):
         # Is it jumping?
         self.jumping = False
 
-        # Setting boundaries
+        # Setting boundaries for jumping
         self.platform_y = ground_y
 
         # Settings speeds
-        self.x_speed = 12
-        self.velocity_index = 0
-
-        # Did player cast spell?
-        self.castingSpell = False
+        self.x_speed = 4
+        self.speed_index = 0
 
         all_sprites_list.add(self)
 
     def do_jump(self):
         if self.jumping:
             # Changing y pos at velocity index speed
-            self.y += velocity[self.velocity_index]
-            self.velocity_index += 1
-            # Minus one bc len() returns 20, velocity list ends at index 19
-            # If speed index is > len(list), max speed reached, return index 19
-            if self.velocity_index >= len(velocity) - 1:
-                self.velocity_index = len(velocity) - 1
-            # Stops movement by settings y pos to platform y pos
+            self.y += jump_speed[self.speed_index]
+            self.speed_index += 1
+
+            if self.speed_index >= len(jump_speed) - 1:
+                self.speed_index = len(jump_speed) - 1
+
+            # Stops movement once boundary hit
             if self.y > self.platform_y:
                 self.y = self.platform_y
                 self.jumping = False
-                self.velocity_index = 0
+                # Resets speed index for next jump
+                self.speed_index = 0
 
     def do_move(self):
         key = pygame.key.get_pressed()
@@ -74,12 +81,8 @@ class Player(pygame.sprite.Sprite):
             self.x -= self.x_speed
 
     def playerUpdate(self):
-        if self.castingSpell:
-            Bullet()
-
         player.rect.x = self.x
         player.rect.y = self.y
-        all_sprites_list.update(player)
 
     def do_player(self):
         self.do_jump()
@@ -91,7 +94,7 @@ class Enemy(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
 
-        self.image = pygame.Surface([sprite_w, sprite_h])
+        self.image = pygame.Surface([sprite_width, sprite_height])
         self.image.fill(BLUE)
         # Gets rect object for collision detection
         self.rect = self.image.get_rect()
@@ -112,28 +115,29 @@ class Enemy(pygame.sprite.Sprite):
     def enemyUpdate(self):
         enemy.rect.x = self.x
         enemy.rect.y = self.y
-        all_sprites_list.update(enemy)
+        all_sprites_list.update()
 
     def do_enemy(self):
         self.move()
         self.enemyUpdate()
 
-class Bullet(pygame.sprite.Sprite):
-    """ This class represents the bullet . """
-    def __init__(self):
-        # Call the parent class (Sprite) constructor
+class MagicSpell(pygame.sprite.Sprite):
+    def __init__(self, start_x, start_y):
         super().__init__()
 
-        self.image = pygame.Surface([5, 5])
+        self.image = pygame.Surface([20, 10])
         self.image.fill(GREEN)
         self.rect = self.image.get_rect()
 
-        self.rect.x = player.rect.center[0]
-        self.rect.y = player.rect.center[1]
+        self.rect.x = start_x
+        self.rect.y = start_y
 
-        self.rect.y -= 3
+        active_spells.add(self)
         all_sprites_list.add(self)
 
+    def update(self):
+        self.rect.x += 2
+        print(self.rect.x)
 
 class Background:
     def __init__(self):
@@ -151,8 +155,8 @@ class Background:
             self.bgX2 = self.bg.get_width()
 
     def drawBackground(self):
-        win.blit(self.bg, (self.bgX, 0))  # draws our first bg image
-        win.blit(self.bg, (self.bgX2, 0))  # draws the second bg image
+        screen.blit(self.bg, (self.bgX, 0))  # draws our first bg image
+        screen.blit(self.bg, (self.bgX2, 0))  # draws the second bg image
         pygame.display.update()
 
     def do_background(self):
@@ -162,42 +166,59 @@ class Background:
 
 def keys(self):
     key = pygame.key.get_pressed()
-    if key[K_SPACE] and player.jumping == False:
+    if key[K_SPACE] and player.jumping is False:
         player.jumping = True
-    if key[K_1]:
-        player.castingSpell = True
+    if key[K_1] and len(active_spells) < 1:
+        MagicSpell(player.rect.centerx, player.rect.centery)
+
 
 def collisions():
-    pygame.sprite.spritecollide(player, enemy_list, True)
+    global run
+    global gameOver
+    gameOver = pygame.sprite.spritecollide(player, enemy_list, True)
+    pygame.sprite.groupcollide(active_spells, enemy_list, True, True)
+
+def draw_text(outcome):
+    font = pygame.font.Font('freesansbold.ttf', 32)
+
+    if outcome == "LOSE":
+        text = font.render('Quest Failed', True, WHITE, BLACK)
+    if outcome == "WIN":
+        text = font.render('Quest Completed', True, WHITE, BLACK)
+
+    text_rect = text.get_rect()
+    text_rect.center = (screen_width // 2, screen_height // 2)
+
+    screen.blit(text, text_rect)
+
 
 # Setting speed
-speed = 30
+speed = 60
 clock = pygame.time.Clock()
 
 pygame.init()
 
+# Declaring lists and sprite groups
+gameOver = []
 
 all_sprites_list = pygame.sprite.Group()
+active_spells = pygame.sprite.Group()
+enemy_list = pygame.sprite.Group()
+
 # Object instantiation
 player = Player(100, ground_y)
 background = Background()
 
-enemy_list = pygame.sprite.Group()
-
+# Attributes to be passed to each enemy
 enemy_attr = [[250, ground_y],
              [450, ground_y],
              [850, ground_y],
-             [1100, ground_y]]
+             [950, ground_y]]
 
+# Generates enemy instances
 for enemyconfig in enemy_attr:
     enemy = Enemy(*enemyconfig)
     enemy_list.add(enemy)
-
-def gameOver():
-    dead_list = pygame.sprite.spritecollide(player, enemy_list, True)
-    if len(dead_list) > 0:
-        print("YA LOST")
-
 
 run = True
 
@@ -214,17 +235,36 @@ while run:
 
     player.do_player()
 
-    all_sprites_list.draw(win)
+    collisions()
 
-    gameOver()
+    all_sprites_list.update()
+
+    all_sprites_list.draw(screen)
 
     pygame.display.update()
+
+    if len(gameOver) > 0:
+        draw_text("LOSE")
+        pygame.display.update()
+        time.sleep(2)
+        run = False
+
+    if len(enemy_list) == 0:
+        draw_text("WIN")
+        pygame.display.update()
+        time.sleep(2)
+        run = False
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-            pygame.quit()
-            quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                run = False
+            #run = False
+            # pygame.quit()
+            # quit()
 
 
 
